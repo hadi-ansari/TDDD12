1367626017fe4dd1ebf76517b2fba658bd73f76e
Hadi Ansari <hadan326@student.liu.se>
2022-05-09 21:37:34 +0200

    Done with 3 e (also updated diagram and pdf)

--- lab4/lab4C.sql
+++ lab4/lab4C.sql
@@ -38,19 +38,19 @@ CREATE TABLE Days(Day VARCHAR(10) PRIMARY KEY, WeekdayFactor DOUBLE, Year INTEGE
 
 CREATE TABLE Airport(AirportCode VARCHAR(3) PRIMARY KEY, Country VARCHAR(30), Name VARCHAR(30));
 
-CREATE TABLE Route(RouteID INTEGER AUTO_INCREMENT PRIMARY KEY, RoutePrice DOUBLE, A_Airport_Code VARCHAR(3), D_Airport_Code VARCHAR(3), Year INTEGER, FOREIGN KEY (A_Airport_Code) REFERENCES Airport(AirportCode), FOREIGN KEY (D_Airport_Code) REFERENCES Airport(AirportCode), FOREIGN KEY (Year) REFERENCES Years(Year));
+CREATE TABLE Route(RoutePrice DOUBLE, A_Airport_Code VARCHAR(3), D_Airport_Code VARCHAR(3), Year INTEGER, FOREIGN KEY (A_Airport_Code) REFERENCES Airport(AirportCode), FOREIGN KEY (D_Airport_Code) REFERENCES Airport(AirportCode), FOREIGN KEY (Year) REFERENCES Years(Year), PRIMARY KEY (A_Airport_Code, D_Airport_Code));
 
-CREATE TABLE Weekly_Schedule(ScheduleID INTEGER AUTO_INCREMENT PRIMARY KEY, Time TIME, Route_ID INTEGER, Day VARCHAR(10), Year INTEGER, FOREIGN KEY (Route_ID) REFERENCES Route(RouteID), FOREIGN KEY (Day) REFERENCES Days(Day), FOREIGN KEY (Year) REFERENCES Years(Year));
+CREATE TABLE Weekly_Schedule(ScheduleID INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY, Time TIME, A_Airport_Code VARCHAR(3), D_Airport_Code VARCHAR(3), Day VARCHAR(10), Year INTEGER, FOREIGN KEY (A_Airport_Code) REFERENCES Route(A_Airport_Code), FOREIGN KEY (D_Airport_Code) REFERENCES Route(D_Airport_Code), FOREIGN KEY (Day) REFERENCES Days(Day), FOREIGN KEY (Year) REFERENCES Years(Year));
 
 CREATE TABLE Passenger(PassportNo INTEGER PRIMARY KEY, FirstName VARCHAR(30), LastName VARCHAR(30));
 
 CREATE TABLE ContactPerson(Passport_No INTEGER PRIMARY KEY, Email VARCHAR(30), PhoneNo BIGINT, FOREIGN KEY (Passport_No) REFERENCES Passenger(PassportNo));
 
-CREATE TABLE Flight(FlightNo INTEGER, Schedule_ID INTEGER, Week INTEGER, Passport_No INTEGER, PRIMARY KEY (FlightNo, Schedule_ID), FOREIGN KEY (Passport_No) REFERENCES ContactPerson(Passport_No));
+CREATE TABLE Flight(FlightNo INTEGER NOT NULL AUTO_INCREMENT, Schedule_ID INTEGER, Week INTEGER, AvailableSeats INTEGER DEFAULT 40, PRIMARY KEY (FlightNo, Schedule_ID), FOREIGN KEY (Schedule_ID) REFERENCES Weekly_Schedule(ScheduleID));
 
 CREATE TABLE Customer(CardNo BIGINT PRIMARY KEY, CardHolderName VARCHAR(30));
 
-CREATE TABLE Reservation(ReservationID INTEGER AUTO_INCREMENT PRIMARY KEY, Flight_No INTEGER, FOREIGN KEY (Flight_No) REFERENCES Flight(FlightNo));
+CREATE TABLE Reservation(ReservationID INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY, Flight_No INTEGER, FOREIGN KEY (Flight_No) REFERENCES Flight(FlightNo));
 
 CREATE TABLE ReservedFor(Reservation_ID INTEGER, Passport_No INTEGER, PRIMARY KEY(Reservation_ID, Passport_No), FOREIGN KEY (Reservation_ID) REFERENCES Reservation(ReservationID), FOREIGN KEY (Passport_No) REFERENCES Passenger(PassportNo));
 
@@ -61,6 +61,7 @@ CREATE TABLE Booking(Reservation_ID INTEGER PRIMARY KEY, NoBookedPassenger INTEG
 /*
 PROCEDURES
 */
+
 delimiter //
 CREATE PROCEDURE addYear(IN year INTEGER, IN factor DOUBLE)BEGIN INSERT INTO Years VALUES (year, factor); END;//
 delimiter ;
@@ -70,3 +71,28 @@ delimiter //
 CREATE PROCEDURE addDay(IN year INTEGER, IN day VARCHAR(10), IN factor DOUBLE)BEGIN INSERT INTO Days VALUES (day, factor, year); END;//
 delimiter ;
 
+delimiter //
+CREATE PROCEDURE addDestination(IN airport_code VARCHAR(3), IN name VARCHAR(30), IN country VARCHAR(30)) BEGIN INSERT INTO Airport VALUES (airport_code, name, country); END;//
+delimiter ;
+
+delimiter //
+CREATE PROCEDURE addRoute(IN departure_airport_code VARCHAR(3), IN arrival_airport_code VARCHAR(3), IN year INTEGER, IN routeprice DOUBLE) BEGIN INSERT INTO Route (RoutePrice, A_Airport_Code, D_Airport_Code, Year) VALUES (routeprice, arrival_airport_code, departure_airport_code, year); END;//
+delimiter ;
+
+
+
+delimiter //
+CREATE PROCEDURE addFlight(IN departure_airport_code VARCHAR(3), IN arrival_airport_code VARCHAR(3), IN year INTEGER, IN day VARCHAR(10), IN departure_time Time)
+      BEGIN
+      DECLARE schedule_id INTEGER;
+      DECLARE counter INTEGER;
+      INSERT INTO Weekly_Schedule(Time, A_Airport_Code, D_Airport_Code, Day, Year) VALUES (departure_time, arrival_airport_code, departure_airport_code, day, year);
+      SET schedule_id = (SELECT max(ScheduleID) FROM Weekly_Schedule);
+      SET counter = 1;
+      WHILE counter < 53 DO
+      INSERT INTO Flight (Schedule_ID, Week) VALUES (schedule_id, counter);
+      SET counter = counter + 1;
+      END WHILE; 
+      END//
+
+delimiter ;
